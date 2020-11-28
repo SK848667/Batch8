02f724b29b73988f8d633b5ce9c7fd98da3ddfbf
sklymenko848667 <848667@gmail.com>
2020-11-28 09:47:38 -0500

    selenium HW

--- SeleniumBatch8/src/com/syntax/class02/HW1.java
+++ SeleniumBatch8/src/com/syntax/class02/HW1.java
@@ -1,12 +1,35 @@
 package com.syntax.class02;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HW1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*
 navigate to https://parabank.parasoft.com/parabank/register.htm;jsessionid=B8DD31FF0AFEDBDF4454B27BBC5C05B0%22
 fill out the forms
 click on register
 close the browser
          */
+            System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.navigate().to("https://parabank.parasoft.com/parabank/register.htm;jsessionid=B8DD31FF0AFEDBDF4454B27BBC5C05B0%22");
+            driver.findElement(By.id("customer.firstName")).sendKeys("firstName");
+            driver.findElement(By.name("customer.lastName")).sendKeys("lastName");
+            driver.findElement(By.id("customer.address.street")).sendKeys("123 street");
+            driver.findElement(By.id("customer.address.city")).sendKeys("city");
+            driver.findElement(By.name("customer.address.state")).sendKeys("state");
+            driver.findElement(By.id("customer.address.zipCode")).sendKeys("12345");
+            driver.findElement(By.name("customer.phoneNumber")).sendKeys("555-123-4567");
+            driver.findElement(By.name("customer.ssn")).sendKeys("111223333");
+            driver.findElement(By.id("customer.username")).sendKeys("username");
+            driver.findElement(By.id("customer.password")).sendKeys("password");
+            driver.findElement(By.id("repeatedPassword")).sendKeys("password");
+            Thread.sleep(2000);
+            driver.findElement(By.className("button")).click();
+            Thread.sleep(2000);
+            driver.quit();
+
     }
 }
--- SeleniumBatch8/src/com/syntax/class02/HW2.java
+++ SeleniumBatch8/src/com/syntax/class02/HW2.java
@@ -1,7 +1,11 @@
 package com.syntax.class02;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HW2 {
-    public static void main(String[] args) {
+
 /*
 navigate to fb.com
 click on create new account
@@ -11,7 +15,23 @@ close the pop up
 close the browser
  */
 
-
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.facebook.com");
+        //driver.findElement(By.partialLinkText("Create New account")).click();
+        driver.findElement(By.id("u_0_2")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.name("firstname")).sendKeys("firstname");
+        driver.findElement(By.name("lastname")).sendKeys("lastname");
+        driver.findElement(By.name("reg_email__")).sendKeys("email@email.com");
+        driver.findElement (By.name ( "reg_email_confirmation__" ) ).sendKeys ("email@email.com");
+        driver.findElement(By.id("password_step_input")).sendKeys("password");
+        driver.findElement (By.id ("u_1_s") ).click ();
+        Thread.sleep (2000 );
+        driver.findElement ( By.id ( "u_1_9" ) ).click ();
+        Thread.sleep (2000 );
+        driver.quit();
 
     }
 }

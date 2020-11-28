d79a65f9ace29ae9604f0c195f9b501cbc64acd6
sklymenko848667 <848667@gmail.com>
2020-11-28 14:42:46 -0500

    class30

---
+++ JavaBatch8/src/com/syntax/class30/ArrayListDemo.java
@@ -0,0 +1,22 @@
+package com.syntax.class30;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+public static void main(String[] args) {
+    ArrayList<String> dynamicArray=new ArrayList<>();
+    dynamicArray.add("text1");
+    System.out.println(dynamicArray.size());
+    dynamicArray.add("text2");
+    System.out.println(dynamicArray.size());
+    dynamicArray.add("text3");
+    System.out.println(dynamicArray.size());
+    dynamicArray.add("text4");
+    System.out.println(dynamicArray.size());
+
+
+
+
+}
+}
+
---
+++ JavaBatch8/src/com/syntax/class30/ArrayListDemo2.java
@@ -0,0 +1,17 @@
+package com.syntax.class30;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo2 {
+    public static void main(String[] args) {
+        ArrayList <Integer> integerArrayList = new ArrayList<>();
+        integerArrayList.add(10);
+        integerArrayList.add(12);
+        integerArrayList.add(14);
+        integerArrayList.add(16);
+
+        System.out.println(integerArrayList.size());//size
+        System.out.println(integerArrayList);//sout
+
+    }
+}

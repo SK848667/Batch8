c638a5d938da468450fb9c4057437adb6f9fa46e
sklymenko848667 <848667@gmail.com>
2020-12-04 13:29:34 -0500

    class4

--- SeleniumBatch8/src/com/syntax/class00review/review01/LinkTextAndPartialLinkText.java
+++ SeleniumBatch8/src/com/syntax/class00review/review01/LinkTextAndPartialLinkText.java
@@ -8,31 +8,28 @@ public class LinkTextAndPartialLinkText {
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         // navigate to HRMS web application
-//        driver.get("http://hrmstest.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+        driver.get("http://hrmstest.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
 
         // login to HRMS
         // enter username
-//        driver.findElement(By.id("txtUsername")).sendKeys("admin");
+        driver.findElement(By.id("txtUsername")).sendKeys("admin");
         // enter password
-//        driver.findElement(By.id("txtPassword")).sendKeys("Hum@nhrm123");
+        driver.findElement(By.id("txtPassword")).sendKeys("Hum@nhrm123");
         // click on login button
-//        driver.findElement(By.id("btnLogin")).click();
-//
-//        Thread.sleep(5000);
+        driver.findElement(By.id("btnLogin")).click();
+        Thread.sleep(5000);
 
         // click on Performance button
 
-//        driver.findElement(By.linkText("Recruitment")).click();
+        driver.findElement(By.linkText("Recruitment")).click();
 
         // partialLinkText()  --> is the part of original linkText when it is a long one
 
-//        driver.findElement(By.partialLinkText("Recruitme")).click();
-
-
+        driver.findElement(By.partialLinkText("Recruitme")).click();
 
 
     }

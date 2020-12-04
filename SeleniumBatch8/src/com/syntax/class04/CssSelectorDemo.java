fb7d978f32a028bccf84b78920ae007a4a4e79c6
sklymenko848667 <848667@gmail.com>
2020-12-04 13:27:43 -0500

    class4

--- SeleniumBatch8/src/com/syntax/class04/CssSelectorDemo.java
+++ SeleniumBatch8/src/com/syntax/class04/CssSelectorDemo.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CssSelectorDemo {
 
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://syntaxtechs.com/selenium-practice/index.php");
         driver.findElement(By.cssSelector("a#btn_basic_example")).click();

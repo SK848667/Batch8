710e349fce305ec659270ad0d3fb946c59b8f265
sklymenko848667 <848667@gmail.com>
2020-12-16 20:02:56 -0500

    all

--- JavaBatch8/src/com/syntax/class35/ExcelFileHandlingDemo2.java
+++ JavaBatch8/src/com/syntax/class35/ExcelFileHandlingDemo2.java
@@ -6,12 +6,11 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ExcelFileHandlingDemo2 {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         String path="C:\\Users\\SK\\IntelliJ\\JavaBatch8\\src\\com\\syntax\\class35\\Test.xlsx";
 
         System.out.println(path); //path to the file

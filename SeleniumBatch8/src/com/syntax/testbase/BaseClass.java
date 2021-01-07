6384f02330ba668138aaa540ab92ca259086e61d
sklymenko848667 <848667@gmail.com>
2021-01-06 22:06:04 -0500

    update

--- SeleniumBatch8/src/com/syntax/testbase/BaseClass.java
+++ SeleniumBatch8/src/com/syntax/testbase/BaseClass.java
@@ -1,38 +1,29 @@
-package com.syntax.utils;
+package com.syntax.testbase;
 
-import com.syntax.testbase.BaseClass;
+import com.syntax.utils.ConfigsReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
 import java.util.concurrent.TimeUnit;
 
-public class CommonMethods extends BaseClass {
+public class BaseClass {
 
+    public static WebDriver driver;
 
-
-
-
-
-
-/*
-   protected static WebDriver driver;
-    */
-/**
+    /**
      * this method will open a browser, set up configuration and navigate to the URL
-     *//*
-
+     */
     public static void setUp() {
 
-        ConfigsReader.readProperties("C:\\Users\\SK\\IntelliJ\\SeleniumBatch8\\src\\configs\\configuration.properties");
+        ConfigsReader.readProperties("configs/configuration.properties");
         switch (ConfigsReader.getPropertyValue("browser").toLowerCase()) {
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
                 driver = new ChromeDriver();
                 break;
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
                 driver = new FirefoxDriver();
                 break;
             default:
@@ -43,19 +34,25 @@ public class CommonMethods extends BaseClass {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    */
-/**
-     * this method will close any open browser
-     *//*
+    /**
+     * this method will navigate a website by the given URL
+     * @param url
+     */
+    public static void setUpWithSpecificURL(String url) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 
+    /**
+     * this method will close any open browser
+     */
     public static void tearDown(){
         if(driver!=null){
             driver.quit();
         }
     }
-
-*/
-
-
 
 }
